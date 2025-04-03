4b6770e4d86aaf3f5e8ad9145f1f4337b716e598
illusion931 <799826845@qq.com>
2025-04-03 22:34:45 +0800

    Add files via upload

---
+++ games/word-chain/words.js
@@ -0,0 +1,13 @@
+// 基础英文单词库
+const wordList = [
+    "apple", "elephant", "tiger", "rabbit", "snake", "eagle", "dog", "cat", "fish",
+    "bird", "monkey", "lion", "zebra", "bear", "wolf", "fox", "deer", "goat",
+    "house", "earth", "happy", "young", "quick", "light", "dark", "kind", "new",
+    "orange", "yellow", "green", "blue", "purple", "white", "black", "brown",
+    "table", "chair", "book", "pencil", "paper", "phone", "computer", "window",
+    "music", "dance", "sing", "play", "work", "study", "learn", "teach", "help",
+    "water", "fire", "wind", "rain", "snow", "sun", "moon", "star", "cloud",
+    "school", "garden", "market", "street", "road", "bridge", "river", "mountain",
+    "friend", "family", "mother", "father", "sister", "brother", "uncle", "aunt",
+    "summer", "winter", "spring", "autumn", "morning", "night", "evening", "day"
+];
